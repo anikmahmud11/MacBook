4bef9f733c08cde743d276eb51edddf322e6b8c9
Anik <baust.cse.160201078@gmail.com>
2021-08-21 20:30:53 +0600

    first commit

---
+++ js/macbook.js
@@ -0,0 +1,85 @@
+//memory
+const unifiedMemory = document.getElementById("8gb-memory");
+const extraUnifiedMemory = document.getElementById("16gb-memory");
+const extraMemoryCost = document.getElementById('extra-memory') ;
+
+unifiedMemory.addEventListener('click',function(){
+  extraMemoryCost.innerText = '0';
+  updateTotal();
+});
+extraUnifiedMemory.addEventListener('click',function(){
+  extraMemoryCost.innerText = '180';
+  updateTotal();
+});
+//storage
+const ssdStorage = document.getElementById("256gb-storage");
+const extraStorage = document.getElementById("512gb-storage");
+const tbStorage = document.getElementById("1Tb-storage");
+const extraStorageCost = document.getElementById('extra-storage') ;
+
+ssdStorage .addEventListener('click',function(){
+  extraStorageCost.innerText = '0';
+  updateTotal();
+});
+extraStorage.addEventListener('click',function(){
+  extraStorageCost.innerText = '100';
+  updateTotal();
+});
+tbStorage.addEventListener('click',function(){
+  extraStorageCost.innerText = '180';
+  updateTotal();
+});
+
+//shipping
+const freeShippinCost = document.getElementById("free-Shipping");
+const onTimeShippinCost = document.getElementById("on-Time-Shipping");
+const deliveryCost = document.getElementById('delevery-cost') ;
+
+freeShippinCost.addEventListener('click',function(){
+  deliveryCost.innerText = '0';
+  updateTotal();
+});
+onTimeShippinCost.addEventListener('click',function(){
+  deliveryCost.innerText = '20';
+  updateTotal();
+});
+//best price
+const bestPrice = document.getElementById('sub-total');
+
+
+//total
+const total = document.getElementById('total-price');
+const footerTotal=document.getElementById('footer-total');
+//update total
+function updateTotal(){
+ const totalMemoryPrice =parseInt(extraMemoryCost.innerText);
+ const fixedPrice = parseInt(bestPrice.innerText);
+ const grandTotal = totalMemoryPrice+fixedPrice;
+ total.innerText=grandTotal;
+ const totalExtraStoragePrice = parseInt(extraStorageCost.innerText);
+ const previousStorageTotalPrice = parseInt(total.innerText);
+ const grandTotalStorage = totalExtraStoragePrice  + previousStorageTotalPrice ;
+ total.innerText=grandTotalStorage ;
+ const totalDeliveryPrice =parseInt( deliveryCost.innerText);
+ const previousTotal = parseInt(total.innerText);
+ const deliveryGrandTotal = totalDeliveryPrice + previousTotal;
+ total.innerText=deliveryGrandTotal;
+ const footerGrandTotal = parseInt(total.innerText);
+ footerTotal.innerText=footerGrandTotal;
+
+}
+
+document.getElementById('promo-apply-btn').addEventListener('click',function(){
+    const promoInput = document.getElementById('promo-input');
+    const promoCode = promoInput.value;
+    // console.log(promoCode);
+if( promoCode == 'stevekaku'){
+  const discount =parseInt ( footerTotal/20);
+  // console.log(discount);
+  const finalDiscount = parseInt(discount.innerText);
+  footerTotal.innerText = finalDiscount;
+  console.log(finalDiscount);
+ 
+}
+
+});
